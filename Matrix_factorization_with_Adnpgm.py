7efc30d52d1a3b326ad0ccb5e37cbef911aefad2
JohnFerdy <JF@example.com>
2025-06-27 14:19:51 +0200

    Add Matrix_factorization_with_Adnpgm updates

--- Matrix_factorization_with_Adnpgm.py
+++ Matrix_factorization_with_Adnpgm.py
@@ -69,8 +69,8 @@ def main():
     X0 = sigma * np.random.randn(m+n, r)
 
     # Params
-    N = 30000
-    L_gd = 1000
+    N     = 5000
+    L_gd  = 1000
     L_nes = 30000
 
     # Συναρτήσεις διεπαφής
